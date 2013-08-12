2e97525bf29fc1b7c810779de801fa59b8e29ffb
admala mahitha <admalamahitha@admalas-MacBook-Pro.local>
2013-08-11 20:15:53 -0400

    Initialized dealers App

---
+++ Dealers/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Dealers::Application.config.secret_key_base = '0b58ff4cdb47f4ae29fa2912f0062ec60de083b79bbbdea87ad308457d3818dadf13e8624972b16d76e4f34e4324bb24137a34c5a4d09523635254bb6f9f9c46'
